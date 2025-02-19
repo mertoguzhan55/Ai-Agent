a975ea930c3f0f32c869a9416d999c90f383000f
mert <55oguzhan.mert@gmail.com>
2025-02-19 22:14:53 +0300

    logs and config is written. App.py is changed, loguru is added into requirements.txt

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from app.dependency import Dependency
 from app.custom_agent import CustomAgent
+from app.config import Configs
+from app.logger import Logger
 
-def main():
+def main(args, configs):
+    logger = Logger(**configs["logger"])
+
+    logger.debug("############ MODEL TRAINING CONFIGURATIONS ############")
+    logger.debug(configs)
+
     dep = Dependency("Halit", 25)
     agent = CustomAgent()
     agent.run(dep)
 
 
 if __name__ == "__main__":
-    main()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--environment", type=str)
+    parser.add_argument("--test", action= "store_true")
+
+
+    args = parser.parse_args()
+
+    configs = Configs().load(config_name=args.environment)
+
+    main(args, configs)
